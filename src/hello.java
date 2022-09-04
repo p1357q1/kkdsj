e0e5b4e049480dace15772f6335006d4dd9647cd
xx <Layne@atguigu.com>
2022-09-04 09:54:17 +0800

    first

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+public class hello {
+
+        public static void main(String [] args){
+            System.out.println("hello git");
+            System.out.println("hello git2");
+            System.out.println("hello git3");
+            System.out.println("hello git4");
+            System.out.println("hello master");
+            System.out.println("hello text");
+
+        }
+    }
+
+
